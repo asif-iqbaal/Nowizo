9e504557dfa43bdca4ba8eba2d78d378ee19584c
asif-iqbaal <mdshami712@gmail.com>
2025-07-07 23:47:38 +0530

    deploy

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,18 +1,24 @@
 import { verifyEmail } from "@/lib/action/emailVerify";
 
-type PageProps = {
-  searchParams: {
-    token?: string;
-  };
-};
+export default async function VerifyEmailPage({
+  searchParams,
+}: {
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
+}) {
+  const resolvedSearchParams = await searchParams;
+  const rawToken = resolvedSearchParams.token;
+  const token = Array.isArray(rawToken) ? rawToken[0] : rawToken;
 
-export default async function VerifyEmailPage({ searchParams }: PageProps) {
-  const token =  searchParams.token;
-  let message = "Invalid request"
+  let message = "Invalid request";
+
   if (token) {
-    const res = await verifyEmail(token)
-    message = res.message
+    const res = await verifyEmail(token);
+    message = res.message;
   }
 
-  return <div className=" w-screen h-screen text-center p-8 text-white flex justify-center items-center">{message}</div>
+  return (
+    <div className="w-screen h-screen text-center p-8 text-white flex justify-center items-center">
+      {message}
+    </div>
+  );
 }
